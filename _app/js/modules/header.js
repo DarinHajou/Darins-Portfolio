17bad834ed86af85dd8316b702fe67d1c084ce7b
DarinHajou <it_related@hotmail.com>
2024-01-28 14:36:08 +0100

    Make hamburger menu close automatically on larger screens

--- _app/js/modules/header.js
+++ _app/js/modules/header.js
@@ -2,7 +2,17 @@ export default function toggleMenuVisibility () {
     const menuButton = document.querySelector(".header__menu-button");
     const nav = document.querySelector(".header__nav");
 
+    // Toggles the active class on menu button click
     menuButton.addEventListener("click", function() {
         nav.classList.toggle("header__nav--active");  
     });
-};
+
+    // Listens to for window resize events
+    window.addEventListener('resize', function() { 
+        // Check if the screen size is greater than 768px
+        if (window.innerWidth >768) {
+            // Remove the active class from the nav
+            nav.classList.remove("header__nav--active");    
+        }
+    });
+}
