020ff9f14481f40ab58cc4996d7f80a718ca899b
Darin Hajou <it_related@hotmail.com>
2024-01-31 13:31:14 +0100

    Feature/add hamburger menu to header (#8)
    * Add Font Awesome CSS style sheet
    
    * Add HTML markup for hamburger menu button
    
    * Adjust the grid column placement for hamburger button
    
    * Adjust grid layout for header and menu button
    
    * Create js file structure including modules folder, main file and header file inside the module folder
    
    * Update nav list styling in header navigation
    
    * Add comment to display flex in '.header__nav .nav__list'
    
    * Add media queries for smaller screen (768px) and hide nav menu by default
    
    * Make the hamburger button (header__nav-button) hidden by default in lager screen and visible in smaller screen
    
    * Implement responsive mobile menu with header__nav--active class
    
    * Fix typo in scrip JS linking in html
    
    * Add toggle menu visibility for hamburger menu in JS
    
    * Link 'toggleMenuVisibility' function to main.js
    
    * Make hamburger menu close automatically on larger screens
    
    * Add 'nav__list' class to header__nav--active in media queries
    
    * Split CSS styles between .nav__list and .header__nav
    
    * Changed the position of the hamburger menu to absolute and added padding to nav__item in smaller screen
    
    * Align items center in the hamburger menu
    
    * Add close button with icon to the mobile navigation menu
    
    * Enhance mobile nav with full-screen overlay and fixed positioning
    
    * Add style and position for close menu button
    
    * Change the nav__link color in opened mobile menu
    
    * Increase the opacity for opened mobile menu
    
    * Hide the close button in default/larger screen
    
    * Add event listener for close button to remove 'header__nav--active' class
    
    * Hide the hamburger icon when 'header__nav--active' is active
    
    * Change the logo and hamburger icon size in smaller screens
    
    * Remove gap 20px in grid.css
    
    * Move the menu style display to the resize function and added else condition
    
    * Re-add menu style button to the menuButton function
    
    * Increase close button size
    
    * Refactor header.css for specificity and add hover effects to nav links.
    
    * Add top margin to logo and nav links for spacing.
    
    * Add horizontal rule (hr) to visually separate header from main content

---
+++ _app/js/modules/header.js
@@ -0,0 +1,28 @@
+export default function toggleMenuVisibility () {
+    const menuButton = document.querySelector(".header__menu-button");
+    const nav = document.querySelector(".header__nav");
+    const closeButton = document.querySelector(".nav__close-button");
+
+    // Toggles the active class on menu button click
+    menuButton.addEventListener("click", function() {
+        nav.classList.toggle("header__nav--active");
+        menuButton.style.display = 'none';
+    });
+    
+    // Listens to for window resize events
+    window.addEventListener('resize', function() { 
+        // Check if the screen size is greater than 768px
+        if (window.innerWidth >768) {
+            // Remove the active class from the nav
+            nav.classList.remove("header__nav--active");
+            menuButton.style.display = 'none';
+        } else {
+            menuButton.style.display = 'block';
+    }
+
+        closeButton.addEventListener("click", function () {
+            nav.classList.remove("header__nav--active");
+            menuButton.style.display = 'block';
+        })
+    });
+}
